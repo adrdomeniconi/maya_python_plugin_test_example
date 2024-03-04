2c8f5325ac9f9ffb0f1c06a2390c8d2f081b28e0
Adriano Domeniconi <adrdomeniconi@gmail.com>
2024-03-04 19:44:19 -0300

    Unit tests for the CenterPointNode class

--- tests/unit/test_center_point_node.py
+++ tests/unit/test_center_point_node.py
@@ -7,14 +7,46 @@ import maya.api.OpenMaya as om
 import center_point_node as plugin_module
 from center_point_node import CenterPointNode
 
-def test_should_not_compute_if_not_dirty():
-    pass
+@fixture
+def center_point_node():
+    return CenterPointNode()
 
-# def test_should_compute_if_dirty():
-#     pass
+def test_should_not_compute_if_not_dirty(center_point_node):
+    plug = CenterPointNode.outputPosAttr
+    dataBlock = Mock()
+    
+    center_point_node.compute(plug, dataBlock)
 
-# def test_should_set_clean_after_computing():
-#     pass
+    dataBlock.outputValue.set3Double.assert_not_called()
 
-# def test_find_center_point():
-#     pass
+def test_should_compute_if_dirty(center_point_node):
+    plug = CenterPointNode.outputPosAttr
+    dataBlock = Mock()
+    outputValue = Mock()
+    dataBlock.outputValue.return_value = outputValue
+    
+    center_point_node.compute(plug, dataBlock)
+
+    outputValue.set3Double.assert_called()
+
+def test_should_set_clean_after_computing(center_point_node):
+    plug = CenterPointNode.outputPosAttr
+    dataBlock = Mock()
+    outputValue = Mock()
+    dataBlock.outputValue.return_value = outputValue
+    
+    center_point_node.compute(plug, dataBlock)
+
+    dataBlock.setClean.assert_called_with(plug)
+
+def test_find_center_point(center_point_node):
+    input_positions = [om.MVector(0.0, 0.0, 0.0), 
+                       om.MVector(0.5, 10.3, 50.8), 
+                       om.MVector(-15.5, -8.0, -30.0), 
+                       om.MVector(34.4, 56.66, -45.0)]
+    
+    expected_result = om.MVector(4.850, 14.740, -6.050)
+
+    result = center_point_node._find_center_point(input_positions)
+
+    assert result == expected_result
